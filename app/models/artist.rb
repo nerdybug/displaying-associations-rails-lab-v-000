2a3b8eb1fd83cac9dc11254b1514477935bfe233
Meredith Reece <mardebugger@gmail.com>
2018-07-25 03:13:55 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,11 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def song_count
-    total = self.songs.count
-    if total == 1
-      "#{total} song"
-    else
-      "#{total} songs"
-    end
+    self.songs.count
   end
 end
